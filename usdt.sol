e866797c7c0e8f10346c2cc537f954d95ae7b6ce
Abdalkarim1979 <avrmicrotech@gmail.com>
2024-10-08 12:07:17 +0300

    Smart Contract USDC and USDT

---
+++ usdt.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDC is ERC20, Ownable {
+    constructor() ERC20("USDT Coin", "USDT") Ownable(msg.sender) {
+        _mint(msg.sender, 10000000 * 10 ** decimals()); // إصدار مليون عملة USDC للمالك
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
